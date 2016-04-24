4205ed1ebdbf21f4316d9f6a8cf329192dad53a8
Michał Czarnecki <mich.czarnecki@o2.pl>
2016-04-24 18:58:43 +0200

    Initial commit

---
+++ src/main/java/com/stocking/StocKingApplication.java
@@ -0,0 +1,12 @@
+package com.stocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocKingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocKingApplication.class, args);
+	}
+}
